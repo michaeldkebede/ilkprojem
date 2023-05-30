9a9dcde7b33d03798e221fe736064e61ba14cd1e
Michael Dereje K <Michaeldkebede@gmail.com>
2023-05-30 21:19:35 +0300

    Update ilkproje.py
    kod ekledim

--- ilkproje.py
+++ ilkproje.py
@@ -1,1 +1,10 @@
 
+
+modern_kelime_sozluk = {"Lol":"kahka atmak", "cringe":"utandirici"}
+cevap = input("hangi kelimenin tanimini ogrenmek istiyorsun?")
+
+if cevap.lower() in modern_kelime_sozluk:
+    print(modern_kelime_sozluk[cevap])
+
+else:
+    print("kelime sozlukte yok")
